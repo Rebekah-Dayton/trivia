85478daf47f0b1d1e74f9854e6c7ed6f8f7f0a64
Ubuntu <ubuntu@ip-172-31-2-28.us-west-1.compute.internal>
2022-12-08 23:49:46 +0000

    quiz

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,214 @@
+var quizQuestions = [
+  {
+      question: "A carnivorous animal eats flesh, what does a nucivorous animal eat?",
+      answers: [
+          {
+              type: "You've got more to learn :)",
+              content: "Nothing"
+          },
+          {
+              type: "You're good at this!",
+              content: "Nuts"
+          },
+          {
+              type: "I'm sure you'll get it next time!",
+              content: "Seaweed"
+          },
+          {
+              type: "Not quite right, let's try again!",
+              content: "Fruit"
+          }
+      ]
+  },
+  {
+      question: "What name is given to all baby marsupials?",
+      answers: [
+          {
+              type: "You've got more to learn :)",
+              content: "Calf"
+          },
+          {
+              type: "You're good at this!",
+              content: "Joey"
+          },
+          {
+              type: "You missed a couple",
+              content: "Cub"
+          },
+          {
+              type: "Not quite right, let's try again!",
+              content: "Pup"
+          }
+      ]
+  },
+  {
+      question: "In Dragon Ball Z, who was the first character to go Super Saiyan 2?",
+      answers: [
+          {
+              type: "You've got more to learn :)",
+              content: "Goku"
+          },
+          {
+              type: "You're good at this!",
+              content: "Gohan"
+          },
+          {
+              type: "You missed a couple",
+              content: "Trunks"
+          },
+          {
+              type: "Not quite right, let's try again!",
+              content: "Vegeta"
+          }
+      ]
+  },
+  {
+      question: "What do the 4 Rings in Audi's Logo represent?",
+      answers: [
+          {
+              type: "You've got more to learn :)",
+              content: "States in which Audi makes the most sales"
+          },
+          {
+              type: "You're good at this!",
+              content: "Previously independent automobile manufacturers"
+          },
+          {
+              type: "You missed a couple",
+              content: "Countries in which Audi makes the most sales"
+          },
+          {
+              type: "Not quite right, let's try again!",
+              content: "Main cities vital to Audi"
+          }
+      ]
+  },
+  {
+      question: "The greek god Poseidon was the god of what?",
+      answers: [
+          {
+              type: "You've got more to learn :)",
+              content: "War"
+          },
+          {
+              type: "You're good at this!",
+              content: "The Sea"
+          },
+          {
+              type: "You missed a couple",
+              content: "Fire"
+          },
+          {
+              type: "Not quite right, let's try again!",
+              content: "Sun"
+          }
+      ]
+  },
+  {
+      question: "What stage of development do the majority of eukaryotic cells remain in for most of their life?",
+      answers: [
+          {
+              type: "You've got more to learn :)",
+              content: "Prophase"
+          },
+          {
+              type: "You're good at this!",
+              content: "Interphase"
+          },
+          {
+              type: "You missed a couple",
+              content: "Telophase"
+          },
+          {
+              type: "Not quite right, let's try again!",
+              content: "Stasis"
+          }
+      ]
+  },
+  {
+      question: "What year did \"Attack on Titan\" first air?",
+      answers: [
+          {
+              type: "You've got more to learn :)",
+              content: "2014"
+          },
+          {
+              type: "You're good at this!",
+              content: "2013"
+          },
+          {
+              type: "You missed a couple",
+              content: "2015"
+          },
+          {
+              type: "Not quite right, let's try again!",
+              content: "2012"
+          }
+      ]
+  },
+  {
+      question: "What is the 4th boss in the 1997 video game \"Crash Bandicoot 2: Cortex Strikes Back\"?",
+      answers: [
+          {
+              type: "You've got more to learn :)",
+              content: "Dr. Neo Cortex"
+          },
+          {
+              type: "You're good at this!",
+              content: "Dr. N. Gin"
+          },
+          {
+              type: "You missed a couple",
+              content: "Tiny Tiger"
+          },
+          {
+              type: "Not quite right, let's try again!",
+              content: "Komodo Brothers"
+          }
+      ]
+  },
+  {
+      question: "What film did James Cameron's Avatar dethrone as the highest-grossing film ever?",
+      answers: [
+          {
+              type: "You've got more to learn :)",
+              content: "Star Wars"
+          },
+          {
+              type: "You're good at this!",
+              content: "Titanic"
+          },
+          {
+              type: "You missed a couple",
+              content: "Jaws"
+          },
+          {
+              type: "Not quite right, let's try again!",
+              content: "Gone with the Wind"
+          }
+      ]
+  },
+  {
+      question: "Which of these Cherry MX mechanical keyboard switches is both tactile and clicky?",
+      answers: [
+          {
+              type: "You've got more to learn :)",
+              content: "Cherry MX Black"
+          },
+          {
+              type: "You're good at this!",
+              content: "Cherry MX Blue"
+          },
+          {
+              type: "You missed a couple",
+              content: "Cherry MX Brown"
+          },
+          {
+              type: "Not quite right, let's try again!",
+              content: "Cherry MX Red"
+          }
+      ]
+  }
+];
+
+export default quizQuestions;
